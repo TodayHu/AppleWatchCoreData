1cfae35b72be0abf558e1f0c6d97f2fe009e7e86
Abu Mathew Thoppan <apple@Apples-MacBook-Air.local>
2015-04-15 10:20:44 +0530

    Phone app working perfectly

--- DateSaverKit/DataAccess.swift
+++ DateSaverKit/DataAccess.swift
@@ -61,39 +61,40 @@ public lazy var applicationDocumentsDirectory: NSURL = {
     return urls[urls.count-1] as NSURL
     }()
 
-    public lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource("DateSaver", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        
+        let proxyBundle = NSBundle(identifier: "com.qbuser.DateSaver")
+        let modelURL = proxyBundle?.URLForResource("DateSaver", withExtension: "momd")!
+        
+        return NSManagedObjectModel(contentsOfURL: modelURL!)!
         }()
 
-public lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
-    // The persistent store coordinator for the application. This implementation creates and return a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-    // Create the coordinator and store
-    
-    let containerPath = NSFileManager.defaultManager().containerURLForSecurityApplicationGroupIdentifier("group.com.qburst.toDoListAppGroup")?.path
-    let sqlitePath = NSString(format: "%@/%@", containerPath!, "DateSaver")
-    let url = NSURL(fileURLWithPath: sqlitePath)
-    
-    var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-    var error: NSError? = nil
-    var failureReason = "There was an error creating or loading the application's saved data."
-    if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
-        coordinator = nil
-        // Report any error we got.
-        var dict = [String: AnyObject]()
-        dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-        dict[NSLocalizedFailureReasonErrorKey] = failureReason
-        dict[NSUnderlyingErrorKey] = error
-        error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-        // Replace this with code to handle the error appropriately.
-        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        NSLog("Unresolved error \(error), \(error!.userInfo)")
-        abort()
-    }
-    
-    return coordinator
-    }()
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator? = {
+        
+        let containerPath = NSFileManager.defaultManager().containerURLForSecurityApplicationGroupIdentifier("group.com.qburst.toDoListAppGroup")?.path
+         let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("ShareCoreData.sqlite")   
+        
+        let  model = self.managedObjectModel;
+        
+        var coordinator: NSPersistentStoreCoordinator? = NSPersistentStoreCoordinator(managedObjectModel: model)
+        var error: NSError? = nil
+        
+        var failureReason = "There was an error creating or loading the application's saved data."
+        if coordinator!.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil, error: &error) == nil {
+            coordinator = nil
+            // Report any error we got.
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            dict[NSUnderlyingErrorKey] = error
+            error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            
+            NSLog("Unresolved error \(error), \(error!.userInfo)")
+            abort()
+        }
+        
+        return coordinator
+        }()
 
 public lazy var managedObjectContext: NSManagedObjectContext? = {
     // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
